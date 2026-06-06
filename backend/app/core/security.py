898f30c7ffd58cf01e2151170bdc70caf88634fe
Andres Calderon <andrescapital.dev@gmail.com>
2026-06-06 18:52:44 -0500

    Ruta usuario protegida

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,7 +6,7 @@ from jose import jwt
 
 from datetime import datetime
 from datetime import timedelta
-
+from jose import JWTError
 # =========================================
 # ENCRIPTAR PASSWORDS
 # =========================================
@@ -88,3 +88,22 @@ def create_access_token(data: dict):
     )
 
     return encoded_jwt
+
+def verify_token(token: str):
+
+    try:
+
+        payload = jwt.decode(
+
+            token,
+
+            SECRET_KEY,
+
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+
+        return None
