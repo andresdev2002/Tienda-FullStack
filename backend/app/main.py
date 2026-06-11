9a31a091910be8ddbda42bf147d03f77c586d450
Andres Calderon <andrescapital.dev@gmail.com>
2026-06-11 17:33:42 -0500

    Dashboard consumiendo datos reales desde la API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.database import Base
+
 from app.routes.categoria_routes import router as categoria_router
 from app.routes.productos_routes import router as productos_router
 from app.routes.proveedor_routes import router as proveedor_router
 from app.routes.venta_routes import router as venta_router
 from app.routes.usuario_routes import router as usuario_router
-from app.routes.movimiento_inventario_routes import (
-    router as movimiento_router
-)
-from app.models import *
+from app.routes.movimiento_inventario_routes import router as movimiento_router
 from app.routes.inventario_routes import router as inventario_router
 from app.routes.clientes_routes import router as clientes_router
-from app.routes.dashboard_routes import (
-    router as dashboard_router
-)
-# ========================================
-# CREAR TABLAS
-# =========================================
+from app.routes.dashboard_routes import router as dashboard_router
 
-Base.metadata.create_all(
-    bind=engine
-)
-
+from app.models import *
 
 # =========================================
 # APP
@@ -33,6 +24,23 @@ app = FastAPI(
     title="API Inventario Profesional"
 )
 
+# =========================================
+# CORS
+# =========================================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# =========================================
+# CREAR TABLAS
+# =========================================
+
+Base.metadata.create_all(bind=engine)
 
 # =========================================
 # HOME
@@ -40,10 +48,13 @@ app = FastAPI(
 
 @app.get("/")
 def home():
-
     return {
         "message": "API funcionando correctamente"
     }
+
+# =========================================
+# ROUTERS
+# =========================================
 
 app.include_router(categoria_router)
 app.include_router(productos_router)
@@ -54,4 +65,3 @@ app.include_router(usuario_router)
 app.include_router(inventario_router)
 app.include_router(clientes_router)
 app.include_router(dashboard_router)
-
